b29ba0455e429ce1b3da97c842fa855d1258e7dc
gkalluru77 <girinath.kalluru@gmail.com>
2023-11-01 17:20:01 +0530

    Expense project = Frontend interface

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 
-cp backend.servicee /etc/systemd/system/backend.servicee
+cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 

72c0b25f94f7513d55a7ea3d818e101d60f84da9
gkalluru77 <girinath.kalluru@gmail.com>
2023-11-03 17:02:53 +0530

    Expense project = Frontend interface

--- backend.sh
+++ backend.sh
@@ -2,32 +2,33 @@ log_file="/tmp/expense.log"
 color="\e[31m"
 
 echo -e "${color} Disable node js default version \e[0m"
+
 dnf module disable nodejs &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} Enable node js 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} Install node js 18 version \e[0m"
 dnf install nodejs -y &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} copying backend service \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} adding application user \e[0m"
 useradd expense &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} creating application directory \e[0m"
 mkdir /app &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} Downloading application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
- echo $?
+echo $?
 
 echo -e "${color} extract application content \e[0m"
 cd /app &>>$log_file

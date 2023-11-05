d5a755f4a7ac4a747c6eef2b7a24c80c3e01ddae
gkalluru77 <girinath.kalluru@gmail.com>
2023-11-05 21:10:07 +0530

    Expense project = Frontend interface

--- common.sh
+++ common.sh
@@ -3,9 +3,9 @@ color="\e[32m"
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "${color} SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILURE \e[0m"
-       exit 2
+      echo -e "${color} SUCCESS \e[0m"
+      else
+        echo -e "\e[31m FAILURE \e[0m"
+         exit 2
   fi
 }

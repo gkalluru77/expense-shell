1f76e7dac79784e08f874f874a5a1c3f80d96464
gkalluru77 <girinath.kalluru@gmail.com>
2023-11-05 17:18:55 +0530

    Expense project = Frontend interface

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,11 @@
 source common.sh
 
+if [ -z "$1" ]; then
+  echo Password input missing
+  exit
+fi
+MYSQL_ROOT_PASSWORD=$1
+
 dnf module disable mysql -y
 status_check
 cp mysql.repo /etc/yum.repos.d/mysql.repo
@@ -9,7 +15,7 @@ status_check
 systemctl enable mysqld
 systemctl start mysqld
 status_check
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD}
 status_check
 
 

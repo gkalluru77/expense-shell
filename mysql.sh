09be8130b3549a8c500c63363ad183bc8de51ce4
gkalluru77 <girinath.kalluru@gmail.com>
2023-11-05 17:10:44 +0530

    Expense project = Frontend interface

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,15 @@
-dnf module disable mysql -y
+source common.sh
 
+dnf module disable mysql -y
+status_check
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 dnf install mysql-community-server -y
-
+status_check
 systemctl enable mysqld
 systemctl start mysqld
-
+status_check
 mysql_secure_installation --set-root-pass ExpenseApp@1
-
+status_check
 
 
